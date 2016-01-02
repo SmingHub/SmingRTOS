5e199e946188017a35c0534f1272c76999ec639a
Richard Antony Burton <richardaburton@gmail.com>
2016-01-02 09:41:03 +0000

    platform specific helper makefiles

--- ESP_RTOS_POC/Makefile-linux.mk
+++ ESP_RTOS_POC/Makefile-linux.mk
@@ -1,15 +1,18 @@
+# default SMING_HOME. Can be overriden.
+SMING_HOME ?= /opt/sming/Sming
+
 # ESP8266 sdk package home directory
 ESP_HOME ?= /opt/esp-open-sdk
 
-# Default COM port
-COM_PORT     ?= /dev/ttyUSB0
+# default COM port
+COM_PORT ?= /dev/ttyUSB0
 
 # base directory of the ESP8266 SDK package, absolute
-SDK_BASE	?= $(ESP_HOME)/sdk
-SDK_TOOLS	 ?= $(SDK_BASE)/tools
+SDK_BASE  ?= /opt/ESP8266_RTOS_SDK
+SDK_TOOLS ?= $(SDK_BASE)/tools
 
-# Other tools mappings
-ESPTOOL		 ?= $(ESP_HOME)/esptool/esptool.py
+# other tools mappings
+ESPTOOL      ?= $(ESP_HOME)/esptool/esptool.py
 KILL_TERM    ?= pkill -9 -f "$(COM_PORT) $(COM_SPEED_SERIAL)" || exit 0
 GET_FILESIZE ?= stat --printf="%s"
 TERMINAL     ?= python -m serial.tools.miniterm $(COM_PORT) $(COM_SPEED_SERIAL)
