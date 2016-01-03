83b59c5f8d26ae91fdc298fa96431349f07f5831
alonewolfx2 <alonewolf07@gmail.com>
2016-01-03 17:34:57 +0200

    Ultrasonic_HCSR04 Library & Example
    Ultrasonic_HCSR04 Library & Example Added.

---
+++ samples/Ultrasonic_HCSR04/app/application.cpp
@@ -0,0 +1,26 @@
+#include <SmingCore.h>
+#include <Libraries/Ultrasonic/Ultrasonic.h>
+
+#define TRIG_PIN 4
+#define ECHO_PIN 5
+
+Timer measureTimer;
+Ultrasonic ultrasonic = Ultrasonic();
+
+void measure()
+{
+	// get distance
+	uint16_t dist = ultrasonic.rangeCM();
+
+	// print the distance
+	Serial.println(dist);
+}
+
+void init()
+{
+	Serial.begin(115200);
+
+	ultrasonic.begin(TRIG_PIN, ECHO_PIN);
+	measureTimer.initializeMs(500,measure).start();
+
+}
