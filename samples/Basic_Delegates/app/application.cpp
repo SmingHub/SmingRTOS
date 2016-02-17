3e07d21c175be5e3989b61d991f5a4ff8f21f61b
hreintke <hreintke@tauri.nl>
2016-02-17 15:40:12 +0100

    Completion of example

--- samples/Basic_Delegates/app/application.cpp
+++ samples/Basic_Delegates/app/application.cpp
@@ -1,19 +1,46 @@
 #include <user_config.h>
 #include <SmingCore.h>
 
-#define LED_PIN 2 // GPIO2
-
-Timer procTimer;
-bool state = true;
-
-void blink()
+class LedBlinker
 {
-	digitalWrite(LED_PIN, state);
-	state = !state;
-}
+
+public :
+	LedBlinker(int reqPin) : ledPin(reqPin) {
+		pinMode(ledPin, OUTPUT);
+	};
+	bool setTimer(int reqInterval) {
+		if (reqInterval <= 0) return false;
+		ledInterval = reqInterval;
+		return true;
+	}
+	void blink(bool reqRun) {
+		if (reqRun) {
+			ledTimer.initializeMs(ledInterval, TimerDelegate(&LedBlinker::ledBlink,this)).start();
+		}
+		else {
+			ledTimer.stop();
+		}
+	}
+	void ledBlink () { ledState = !ledState ; digitalWrite(ledPin, ledState);}
+
+private :
+	int ledPin = 2;
+	Timer ledTimer;
+	int ledInterval = 1000;
+	bool ledState = true;
+};
+
+#define LEDPIN_1  2 // GPIO2
+#define LEDPIN_2  4 // GPIO4
+
+LedBlinker myLed1 = LedBlinker(LEDPIN_1);
+LedBlinker myLed2 = LedBlinker(LEDPIN_2);
 
 void init()
 {
-	pinMode(LED_PIN, OUTPUT);
-	procTimer.initializeMs(1000, blink).start();
+	myLed1.setTimer(1000);
+	myLed1.blink(true);
+	myLed2.setTimer(500);
+	myLed2.blink(true);
 }
+
