075f808582d0688e64359bbc51c0f04e4385284e
Alexander V. Ribchansky <avr39ripe@gmail.com>
2016-03-02 18:14:09 +0200

    Fix WIFI_SSID and WIFI_PWD issue by #ifndef-ining them

--- samples/Basic_WebSkeletonApp/include/configuration.h
+++ samples/Basic_WebSkeletonApp/include/configuration.h
@@ -1,6 +1,12 @@
 #ifndef INCLUDE_CONFIGURATION_H_
 #define INCLUDE_CONFIGURATION_H_
 
+// If you want, you can define WiFi settings globally in Eclipse Environment Variables
+#ifndef WIFI_SSID
+        #define WIFI_SSID "PleaseEnterSSID" // Put you SSID and Password here
+        #define WIFI_PWD "PleaseEnterPass"
+#endif
+
 #include <SmingCore.h>
 
 const char THERM_CONFIG_FILE[] = ".therm.conf"; // leading point for security reasons :)
